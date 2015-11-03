2ce5cd2e31af376721acc5ef2e8755cd77abb692
Tomaž Žniderič <=>
2015-11-03 19:56:02 +0100

    requirments

--- travelcosts/settings.py
+++ travelcosts/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -77,11 +76,14 @@ WSGI_APPLICATION = 'travelcosts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travelcosts',
+        'USER': 'postgres',
+        'PASSWORD': 'blabla123',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
