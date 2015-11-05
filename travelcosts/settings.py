a8d3401b17f8c92cb6f806023efb66c6194345be
Tomaž Žniderič <=>
2015-11-05 18:12:40 +0100

    blabla

--- travelcosts/settings.py
+++ travelcosts/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 
 DATABASES = {}
 
+
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
 
@@ -97,9 +98,7 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
+# Allowall host headers
 ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
