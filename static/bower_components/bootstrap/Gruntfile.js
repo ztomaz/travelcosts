479f8a7ddba973486f9c640d18e73ecfc217a144
Tomaž Žniderič <=>
2015-11-22 15:57:58 +0100

    do not put dist in you gitignore if you have usuable files there

--- static/bower_components/bootstrap/Gruntfile.js
+++ static/bower_components/bootstrap/Gruntfile.js
@@ -146,7 +146,7 @@ module.exports = function (grunt) {
       options: {
         inject: 'js/tests/unit/phantom.js'
       },
-      files: 'js/tests/template.html'
+      files: 'js/tests/index.html'
     },
 
     less: {
@@ -376,7 +376,7 @@ module.exports = function (grunt) {
           throttled: 10,
           maxRetries: 3,
           maxPollRetries: 4,
-          urls: ['http://127.0.0.1:3000/js/tests/template.html?hidepassed'],
+          urls: ['http://127.0.0.1:3000/js/tests/index.html?hidepassed'],
           browsers: grunt.file.readYAML('grunt/sauce_browsers.yml')
         }
       }
